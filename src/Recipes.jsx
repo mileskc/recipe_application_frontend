43601521d01f758794cd92d07e39cf3f49ad61e0
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-01-13 18:29:50 -0500

    Made react app and recipes page to render without recipes

---
+++ src/Recipes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Recipes extends React.Component {
+    constructor() {
+        super ()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <h1>Hello Recipes! This is the Recipes page</h1>
+        )
+    }
+}
+
+export default Recipes
