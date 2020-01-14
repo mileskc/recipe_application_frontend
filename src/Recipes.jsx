97bba80ebe316d2edbb7c5cf2bcb73b58b4392e2
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-01-13 21:37:08 -0500

    got recipe data from localhost backend to console log to front end

--- src/Recipes.jsx
+++ src/Recipes.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
 class Recipes extends React.Component {
-    constructor() {
-        super ()
-        this.state = {
+  constructor() {
+    super();
+    this.state = {
+      recipes: []
+    };
+  }
+  componentDidMount() {
+    this.getRecipes();
+  }
 
-        }
-    }
+  getRecipes() {
+    fetch("http://localhost:3000/recipes")
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error));
+  }
 
-    render() {
-        return (
-            <h1>Hello Recipes! This is the Recipes page</h1>
-        )
-    }
+  render() {
+    return <h1>Hello Recipes! This is the Recipes page</h1>;
+  }
 }
 
-export default Recipes
+export default Recipes;
